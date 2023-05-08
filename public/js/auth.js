579b6eacfcd1398929ee83f4d3267b16e09608fd
LUIS TORREALBA <luisdavid@gmail.com>
2023-05-08 14:57:51 -0400

    cambio

--- public/js/auth.js
+++ public/js/auth.js
@@ -4,7 +4,7 @@ const miFormulario = document.querySelector('form');
 
 const url = ( window.location.hostname.includes('localhost') )
             ? 'http://localhost:8080/api/auth/'
-            : 'https://restserver-curso-fher.herokuapp.com/api/auth/';
+            : 'https://casagribackend.netlify.app/api/auth/';
 
 
 
